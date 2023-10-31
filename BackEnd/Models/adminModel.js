0d25feb3db1caaf6ab913847bf2cfa1859c73483
IAmBill030202 <nhantvde160428@fpt.edu.vn>
2023-10-31 09:33:51 +0700

    Update admin controller

--- BackEnd/Models/adminModel.js
+++ BackEnd/Models/adminModel.js
@@ -6,19 +6,13 @@ const adminSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-  adminUsername: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  role: {
-    type: String,
-    default: 'admin',
-  },
   fullname: {
     type: String,
     required: true,
   },
+  isActive:{
+    type: Boolean,
+  }
 }, { collection: 'Admin' });
 
 const Admin = mongoose.model('Admin', adminSchema);
