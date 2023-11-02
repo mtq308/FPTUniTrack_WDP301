1bcfcd5d658cf4295c623239336d8a69bb6b705e
VawnHuy <huy820op@gmail.com>
2023-11-02 21:57:34 +0700

    semester connect frontend backend

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -7,11 +7,13 @@ const lecturerRoutes = require('./routes/lecturerRoutes');
 const adminRoutes = require('./routes/adminRoutes');
 const authRoutes = require('./routes/authRoutes');
 const swaggerRoute = require('./configs/swaggerRoute');
+const semesterRoutes = require('./routes/semesterRoutes');
 const app = express();
-
+const cors = require('cors');
 // Middleware
 app.use(bodyParser.json());
 app.use(express.json());
+app.use(cors());
 // Database connection
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -22,11 +24,14 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
   });
 
 // Routes
-app.use('/auth', authRoutes); 
-app.use('/student', studentRoutes); 
-app.use('/lecturer', lecturerRoutes); 
-app.use('/admin', adminRoutes); 
+app.use('/auth', authRoutes);
+app.use('/student', studentRoutes);
+app.use('/lecturer', lecturerRoutes);
+app.use('/admin', adminRoutes);
 app.use('/', swaggerRoute);
+app.use('/semester', semesterRoutes);
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
