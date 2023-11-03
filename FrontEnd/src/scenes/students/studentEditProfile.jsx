4bb66acb840628187a8834b3e409eac43841dab2
mtq308 <tranminhquoc945@gmail.com>
2023-11-03 10:38:53 +0700

    student detail and student edit profile complete for front-end

--- FrontEnd/src/scenes/students/studentEditProfile.jsx
+++ FrontEnd/src/scenes/students/studentEditProfile.jsx
@@ -16,7 +16,14 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
 import dayjs from "dayjs";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 import { useNavigate } from "react-router-dom";
 import Header from "../../components/Header";
 
@@ -27,10 +34,14 @@ const StudentEditProfile = () => {
   const navigate = useNavigate();
   // Get the studentId from the URL using useParams
   const { studentId } = useParams();
-
+  const [specialization, setSpecialization] = React.useState("");
   // Find the student data by studentId
   const student = studentsData.find((student) => student.id === studentId);
   const dateOfBirth = dayjs(student.DateOfBirth);
+
+  const handleChange = (event) => {
+    setSpecialization(event.target.value);
+  };
   console.log(typeof dateOfBirth);
   console.log(dateOfBirth);
 
@@ -74,26 +85,56 @@ const StudentEditProfile = () => {
             </Stack>
             <Stack direction="row" alignItems="center" sx={{ ml: 10 }}>
               <Typography variant="h5">Gender:</Typography>
-              <TextField
-                disabled
-                label={student.Gender ? "Male" : "Female"}
-                InputProps={{
-                  readOnly: true,
-                }}
+              {/* <TextField
+                defaultValue={student.Gender ? "Male" : "Female"}
                 sx={{ ml: 12.8, width: 80 }}
                 size="small"
-              />
+              /> */}
+              <FormControl sx={{ ml: 9.9 }}>
+                <Stack direction="row">
+                  <RadioGroup
+                    defaultValue={student.Gender ? "male" : "female"}
+                    name="radio-buttons-group"
+                    sx={{ ml: 3 }}
+                  >
+                    <Stack direction="row">
+                      <FormControlLabel
+                        value="female"
+                        control={
+                          <Radio
+                            sx={{
+                              "&, &.Mui-checked": {
+                                color: "silver",
+                              },
+                            }}
+                          />
+                        }
+                        label="Female"
+                      />
+                      <FormControlLabel
+                        value="male"
+                        control={
+                          <Radio
+                            sx={{
+                              "&, &.Mui-checked": {
+                                color: "silver",
+                              },
+                            }}
+                          />
+                        }
+                        label="Male"
+                      />
+                    </Stack>
+                  </RadioGroup>
+                </Stack>
+              </FormControl>
             </Stack>
           </Stack>
           <Stack direction="row" sx={{ mt: 3 }}>
             <Stack direction="row" alignItems="center">
               <Typography variant="h5">ID Card:</Typography>
               <TextField
-                disabled
-                label={student.IDCard}
-                InputProps={{
-                  readOnly: true,
-                }}
+                defaultValue={student.IDCard}
                 sx={{ ml: 7.1 }}
                 size="small"
               />
@@ -110,11 +151,7 @@ const StudentEditProfile = () => {
           <Stack direction="row" alignItems="center" sx={{ mt: 3 }}>
             <Typography variant="h5">Address:</Typography>
             <TextField
-              disabled
-              label={student.Address}
-              InputProps={{
-                readOnly: true,
-              }}
+              defaultValue={student.Address}
               sx={{ ml: 6.7, width: 644 }}
               size="small"
             />
@@ -123,11 +160,8 @@ const StudentEditProfile = () => {
             <Stack direction="row" alignItems="center">
               <Typography variant="h5">Email:</Typography>
               <TextField
-                disabled
-                label={student.Email}
-                InputProps={{
-                  readOnly: true,
-                }}
+                defaultValue={student.Email}
+                type="email"
                 sx={{ ml: 9, width: 250 }}
                 size="small"
               />
@@ -135,11 +169,7 @@ const StudentEditProfile = () => {
             <Stack direction="row" alignItems="center" sx={{ ml: 4 }}>
               <Typography variant="h5">Student username:</Typography>
               <TextField
-                disabled
-                label={student.StudentUsername}
-                InputProps={{
-                  readOnly: true,
-                }}
+                defaultValue={student.StudentUsername}
                 sx={{ ml: 3 }}
                 size="small"
               />
@@ -148,27 +178,63 @@ const StudentEditProfile = () => {
           <Stack direction="row" sx={{ mt: 3 }}>
             <Stack direction="row" alignItems="center">
               <Typography variant="h5">Specialization:</Typography>
-              <TextField
-                disabled
-                label={student.Specialization}
-                InputProps={{
-                  readOnly: true,
-                }}
-                sx={{ ml: 1.7, width: 50 }}
-                size="small"
-              />
+              <FormControl sx={{ ml: 1.7, width: 70 }}>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  defaultValue={student.Specialization}
+                  size="small"
+                  onChange={handleChange}
+                >
+                  <MenuItem value="SE">SE</MenuItem>
+                  <MenuItem value="IA">IA</MenuItem>
+                  <MenuItem value="GD">GD</MenuItem>
+                  <MenuItem value="AI">AI</MenuItem>
+                  <MenuItem value="IB">IB</MenuItem>
+                  <MenuItem value="IS">IS</MenuItem>
+                </Select>
+              </FormControl>
             </Stack>
-            <Stack direction="row" alignItems="center" sx={{ ml: 28.9 }}>
+            <Stack direction="row" alignItems="center" sx={{ ml: 26.4 }}>
               <Typography variant="h5">Is active:</Typography>
-              <TextField
-                disabled
-                label={student.IsActive ? "Yes" : "No"}
-                InputProps={{
-                  readOnly: true,
-                }}
-                sx={{ ml: 12.1, width: 50 }}
-                size="small"
-              />
+              <FormControl sx={{ ml: 9.2 }}>
+                <Stack direction="row">
+                  <RadioGroup
+                    defaultValue={student.IsActive ? "Yes" : "No"}
+                    name="radio-buttons-group"
+                    sx={{ ml: 3 }}
+                  >
+                    <Stack direction="row">
+                      <FormControlLabel
+                        value="No"
+                        control={
+                          <Radio
+                            sx={{
+                              "&, &.Mui-checked": {
+                                color: "silver",
+                              },
+                            }}
+                          />
+                        }
+                        label="No"
+                      />
+                      <FormControlLabel
+                        value="Yes"
+                        control={
+                          <Radio
+                            sx={{
+                              "&, &.Mui-checked": {
+                                color: "silver",
+                              },
+                            }}
+                          />
+                        }
+                        label="Yes"
+                      />
+                    </Stack>
+                  </RadioGroup>
+                </Stack>
+              </FormControl>
             </Stack>
           </Stack>
           <Box sx={{ mt: 5 }}>
@@ -189,7 +255,7 @@ const StudentEditProfile = () => {
                 },
               }}
             >
-              Edit student profile
+              Save
             </Button>
             <Button
               variant="contained"
