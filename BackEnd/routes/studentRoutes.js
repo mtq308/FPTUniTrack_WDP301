615617be95b23ef3e018b0c0c7ae053697acd4bf
toumtam2100 <toumtampde160576@fpt.edu.vn>
2023-11-15 00:23:45 +0700

    completed merge

--- BackEnd/routes/studentRoutes.js
+++ BackEnd/routes/studentRoutes.js
@@ -11,8 +11,8 @@ router.get('/profile/:id', verifyToken, studentController.studentProfile);
 router.get('/getStudentClasses/:id', verifyToken, studentController.getStudentClasses);
 router.get('/getStudentSubjectId/:id', verifyToken, studentController.getStudentBySubjectID)
 router.post('/getSlotsOfWeek', studentController.getSlotsByWeekNumber);
-router.post('/getGrade', studentController.getGrade);
-router.post('/getSubjectByStudentId', studentController.getSubjectIdByStudentId);
+// router.post('/getGrade', studentController.getGrade);
+// router.post('/getSubjectByStudentId', studentController.getSubjectIdByStudentId);
 
 //Routes for subject - LECTURER with READ subject privillege only
 //Get all subjects
